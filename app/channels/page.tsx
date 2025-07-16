cf9622848a356d508b40d47547a876bbce73308e
Musemerwe <muscharles49@gmail.com>
2025-07-16 05:49:00 +0300

    Update page.tsx

--- app/channels/page.tsx
+++ app/channels/page.tsx
@@ -6,7 +6,7 @@ const channelData = [
   { name: "music", src: "/channels/music channel.png", slug: 'channels/music'},
   { name: "movies", src: "/channels/movie channel.png", slug: 'channels/movies'},
   { name: "voices", src: "/channels/voices channel.png", slug: 'channels/voices'},
-  { name: "gospel", src: "/channels/gospel channel.png", slug: 'channels/gosepl'},
+  { name: "gospel", src: "/channels/gospel channel.png", slug: 'channels/gospel'}, // fixed typo here
   { name: "nature", src: "/channels/nature channel.png", slug: 'channels/nature'},
   { name: "discovery", src: "/channels/discovery channel.png", slug: 'channels/discovery'},
   { name: "fashion", src: "/channels/fashion channel.png", slug: 'channels/fashion'},
@@ -20,18 +20,18 @@ export default function ChannelsPage() {
   return (
     <div className="channel-grid">
       {channelData.map((channel, index) => (
-      <Link href={`/${channel.slug}`}>
-        <div key={index} className="channel-item">
-          <Image
-            src={channel.src}
-            alt={channel.name}
-            width={300}
-            height={300}
-            className="channel-image"
-          />
-          <div className="channel-overlay" />
-          <div className="channel-text">{channel.name}</div>
-        </div>
+        <Link href={`/${channel.slug}`} key={index}>
+          <div className="channel-item">
+            <Image
+              src={channel.src}
+              alt={channel.name}
+              width={300}
+              height={300}
+              className="channel-image"
+            />
+            <div className="channel-overlay" />
+            <div className="channel-text">{channel.name}</div>
+          </div>
         </Link>
       ))}
     </div>
